6d6f912ee25adbc16ff9d9b2e29ca6745f8d449d
BODI <memesaleh92@gmail.com>
2019-08-24 01:32:06 +0300

    add title

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aboutme
+//
+//  Created by BODI💜 on 24/08/2019.
+//  Copyright © 2019 Modhy Saleh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
